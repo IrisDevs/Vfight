710de5b36689187e5b12d2549c2dc2acbec8e858
IrisDevs <iris.giraudet@gmail.com>
2024-01-26 11:26:28 +0100

    page streamer

--- Projet/twitch.js
+++ Projet/twitch.js
@@ -1,7 +1,7 @@
 let clientId = "skvkc1c2djff3m3n2tgfuysw85rrdo";
 let clientSecret = "5wm4kt7gyunbi2iw9n8dfuwxesx5l8";
-const login_id = ["45098797", "133058629", "614928965", "175831187"];
-const pseudo = ["cdawgva", "raisindeloup", "manonmergnat", "ironmouse"];
+const login_id = ["45098797", "133058629", "614928965", "175831187", "31821735", "802882537", "75373669",];
+const pseudo = ["cdawgva", "raisindeloup", "manonmergnat", "ironmouse", "starlopal", "iotadraco", "gius",];
 var x = 0;
 var y = 0;
 
@@ -51,21 +51,24 @@ function getTwitchAuthorization() {
     function renderStreams(data) {
         let { data: streams } = data;
         let streamsContainer = document.querySelector("div.streams");
+
+        streams.forEach((stream) => {
+            let { thumbnail_url: thumbnail, title, viewer_count, user_login, user_id} = stream;
+            let hdThumbnail = thumbnail
+                .replace("{width}", "1280")
+                .replace("{height}", "720");
+                document.getElementById("onstreams").innerHTML += "<a href='https://www.twitch.tv/" + user_login + "'><img src='img/" + user_id + ".jpg'></a>";    
+            });
         
 
-        if (JSON.stringify(data) === '{"data":[],"pagination":{}}'){
-           document.getElementById("offline").innerHTML += "<a href='https://www.twitch.tv/" + pseudo[x] + "'><img src='" + login_id[y] + ".jpg'></a>";
-           console.log(x);    
+      
+           document.getElementById("offline").innerHTML += "<a href='https://www.twitch.tv/" + pseudo[x] + "'><img src='img/" + login_id[y] + ".jpg'></a>";
+           console.log(JSON.stringify(data));   
            x ++;    
            y ++;
-        }
         
-        else if (JSON.stringify(data) !== '{"data":[],"pagination":{}}'){
-            console.log(x);    
-            document.getElementById("onstreams").innerHTML += "<div><img src='" + login_id[y] + ".jpg'></div>";
-            x ++;   
-            y ++;
-        }
+        
+       
               
     }
     
